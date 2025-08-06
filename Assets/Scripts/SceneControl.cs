efed5a69806c884e21f6289274fbbe1c3540695f
YusufSaidTaskin <>
2025-08-06 11:12:01 +0300

    Good

--- Assets/Scripts/SceneControl.cs
+++ Assets/Scripts/SceneControl.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class SceneControl : MonoBehaviour
 {
+    public GameObject panel;
+    private void Start()
+    {
+        panel.SetActive(false);
+    }
     public void GoToPlayScene()
     {
         SceneManager.LoadSceneAsync("Oyun");
@@ -12,5 +17,35 @@ public class SceneControl : MonoBehaviour
     public void GoToMenuScene()
     {
         SceneManager.LoadSceneAsync("Menu");
+    }
+
+    public void RestartPlayScene1()
+    {
+         SceneManager.LoadSceneAsync("Oyun");
+    }
+
+    public void RestartPlayScene2()
+    {
+        SceneManager.LoadSceneAsync("Oyun2");
+    }
+
+    public void Scene1Contunie()
+    {
+        SceneManager.LoadSceneAsync("Oyun");
+    }
+
+    public void Scene2Contunie()
+    {
+        SceneManager.LoadSceneAsync("Oyun2");
+    }
+
+    public void SureBittiScene1()
+    {
+        SceneManager.LoadSceneAsync("Oyun2");
+    }
+
+    public void MenuPanelAc()
+    {
+        panel.SetActive(true);
     }
 }
--- Assets/Scripts/TimerBehaviour.cs
+++ Assets/Scripts/TimerBehaviour.cs
@@ -2,31 +2,40 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 
 public class CountdownTimer : MonoBehaviour
 {
     public TimeHolder timeHolder;
     private int countdownTime;        // Baþlangýç süresi
     public TextMeshProUGUI countdownText;            // UI üzerindeki Text (Legacy UI için)
-
+    public GameObject panel;
     void Start()
     {
         countdownTime = timeHolder.totalTime;
-        StartCoroutine(StartCountdown());
+        StartCoroutine(StartCountdownScene1());
     }
 
-    IEnumerator StartCountdown()
+    IEnumerator StartCountdownScene1()
     {
+        
         int currentTime = countdownTime;
-
-        while (currentTime > 0)
         {
-            countdownText.text = currentTime.ToString();
-            yield return new WaitForSeconds(1f);
-            currentTime--;
-        }
+            
+            while (currentTime > 0)
+            {
+                if (!panel.activeSelf)
+                {
+                    countdownText.text = currentTime.ToString();
+                    yield return new WaitForSeconds(1f);
+                    currentTime--;
+                }
+               
+            }
 
-        countdownText.text = "Süre Doldu!";
-        Debug.Log("Geri sayým bitti!");
+            countdownText.text = "Süre Doldu!";
+            FindAnyObjectByType<SceneControl>().SureBittiScene1();
+        }
+        
     }
 }
